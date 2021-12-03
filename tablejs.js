af7bba05c4fb0b3bf9635fa5d1b0381671ea136e
annemarieakoh <91986153+annemarieakoh@users.noreply.github.com>
2021-12-03 14:27:28 -0500

    Update tablejs.js

--- tablejs.js
+++ tablejs.js
@@ -2,12 +2,9 @@ function displayCourse(){
   var checkbox = document.getElementById("course1");
   var text = document.getElementsByClassName("mechanics");
   if (checkbox.checked == true){
-    $("#show").click(function(){
     $(".mechanics").show();
-  });
-  } else{
-    $("#hide").click(function(){
+  };
+  else{
     $(".mechanics").hide();
-  });
-  }
+  };
 }
