0b90a0b8c9c484ea4b98cb5c07c7d522cf9c9f5f
annemarieakoh <91986153+annemarieakoh@users.noreply.github.com>
2021-12-03 14:44:46 -0500

    Update tablejs.js

--- tablejs.js
+++ tablejs.js
@@ -1,6 +1,61 @@
-function displayCourse(){
+function displayCourseMechanics(){
   var checkbox = document.getElementById("course1");
   var text = document.getElementsByClassName("mechanics");
+  if (checkbox.checked == true){
+    $(text).show();
+  }
+  else{
+    $(text).hide();
+  }
+}
+
+function displayCourseComputerScience(){
+  var checkbox = document.getElementById("course2");
+  var text = document.getElementsByClassName("computerscience");
+  if (checkbox.checked == true){
+    $(text).show();
+  }
+  else{
+    $(text).hide();
+  }
+}
+
+function displayCourseEnglish(){
+  var checkbox = document.getElementById("course3");
+  var text = document.getElementsByClassName("english");
+  if (checkbox.checked == true){
+    $(text).show();
+  }
+  else{
+    $(text).hide();
+  }
+}
+
+function displayCourseMaths(){
+  var checkbox = document.getElementById("course4");
+  var text = document.getElementsByClassName("maths");
+  if (checkbox.checked == true){
+    $(text).show();
+  }
+  else{
+    $(text).hide();
+  }
+}
+
+function displayCourseFreshmanQuiz(){
+  var checkbox = document.getElementById("course5");
+  var text = document.getElementsByClassName("freshmanquiz");
+  if (checkbox.checked == true){
+    $(text).show();
+  }
+  else{
+    $(text).hide();
+  }
+}
+
+function displayCourseWebDev(){
+  var checkbox = document.getElementById("course6");
+  var text = document.getElementsByClassName("webdev");
   if (checkbox.checked == true){
     $(text).show();
   }
