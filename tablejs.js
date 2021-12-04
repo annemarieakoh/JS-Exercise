6b817f3624ea3131eb2953b9ee5d703f06ba6b5f
annemarieakoh <91986153+annemarieakoh@users.noreply.github.com>
2021-12-04 00:20:16 -0500

    Update tablejs.js

--- tablejs.js
+++ tablejs.js
@@ -2,16 +2,13 @@ function displayCourseMechanics(){
   var checkbox = document.getElementById("course1");
   var text = document.getElementsByClassName("mechanics");
   var extra = document.getElementsByClassName("extracellmechanics");
-  var extracomputerscience = document.getElementsByClassName("extracellcomputerscience");
   if (checkbox.checked == true){
     $(text).show(); 
     $(extra).hide();
-    $(extracomputerscience).show();
   }
   else{
     $(text).hide(); 
     $(extra).show();
-    $(extracomputerscience).show();
   }
 }
 
@@ -24,16 +21,13 @@ function displayCourseComputerScience(){
   var checkbox = document.getElementById("course2");
   var text = document.getElementsByClassName("computerscience");
   var extra = document.getElementsByClassName("extracellcomputerscience");
-  var extramechanics = document.getElementsByClassName("extracellmechanics");
   if (checkbox.checked == true){
     $(text).show();
     $(extra).hide();
-    $(extramechanics).show();
   }
   else{
     $(text).hide();
     $(extra).show();
-    $(extramechanics).show();
   }
 }
 
